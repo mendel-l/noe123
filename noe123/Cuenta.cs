9c46a45f73f4476a91549539dd54bafe5c900869
m3mes <m3mes@KERRY>
2023-04-16 16:52:25 -0600

    Agregar archivos de proyecto.

---
+++ noe123/Cuenta.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace noe123
+{
+    internal class Cuenta
+    {
+        decimal ValorHaber { get; set; }
+        decimal ValorDebe { get; set; }
+        holaCuenta count { get; set; }
+    }
+
+    public enum TipoCuenta
+    {
+        Activo,
+        Pasivo,
+        Gasto,
+        Ganancia,
+        Capital
+    }
+
+    public enum TipoCuentaEspecial
+    {
+        CapitalAcciones,
+        Ventas,
+        ReservaLegal,
+        UtilidadRetenida
+    }
+
+    public class holaCuenta
+    {
+        public string Nombre { get; set; }
+        public TipoCuenta Tipo { get; set; }
+        public TipoCuentaEspecial TipoEspecial { get; set; }
+    }
+}
+
---
+++ noe123/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace noe123
+{
+    public partial class Form1 : Form
+    {
+        List<holaCuenta> saldos = new List<holaCuenta>();
+        public Form1()
+        {
+            InitializeComponent();
+            // Configurar el DataGridView
+            SaldosDataGridView.AutoGenerateColumns = true;
+            SaldosDataGridView.DataSource = saldos;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            holaCuenta nuevaCuenta = new holaCuenta();
+            nuevaCuenta.Nombre = NombreComboBox.Text;
+            nuevaCuenta.Tipo = (TipoCuenta)TipoComboBox.SelectedIndex;
+            nuevaCuenta.TipoEspecial = (TipoCuentaEspecial)TipoEspecialComboBox.SelectedIndex;
+
+            // Agregar la nueva cuenta a la lista de saldos
+            saldos.Add(nuevaCuenta);
+
+            // Actualizar el DataGridView
+            SaldosDataGridView.DataSource = null;
+            SaldosDataGridView.DataSource = saldos;
+        }
+    }
+}
